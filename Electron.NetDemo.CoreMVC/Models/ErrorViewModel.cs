1fe84b6a968b9a8bfe048b3b1baf69a49849ab9e
Leon <leon.id@qq.com>
2020-01-08 14:36:26 +0800

    添加CoreMVC项目；

---
+++ Electron.NetDemo.CoreMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Electron.NetDemo.CoreMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
